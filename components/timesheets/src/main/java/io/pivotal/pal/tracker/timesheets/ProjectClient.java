d097fb1328722375d60355dff018fdb66f0ef1f9
savitha <sridhar.savitha.wipro.com>
2019-02-01 10:58:00 +0530

    first push-lab3 1st feb

--- components/timesheets/src/main/java/io/pivotal/pal/tracker/timesheets/ProjectClient.java
+++ components/timesheets/src/main/java/io/pivotal/pal/tracker/timesheets/ProjectClient.java
@@ -1,18 +1,32 @@
 package io.pivotal.pal.tracker.timesheets;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.client.RestOperations;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ProjectClient {
 
     private final RestOperations restOperations;
-    private final String endpoint;
+    private final String registrationServerEndpoint;
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+    private final Map<Long, ProjectInfo> projectsCache = new ConcurrentHashMap<>();
 
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
-        this.restOperations = restOperations;
-        this.endpoint = registrationServerEndpoint;
+        this.restOperations= restOperations;
+        this.registrationServerEndpoint = registrationServerEndpoint;
     }
 
+    @HystrixCommand(fallbackMethod="getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+        return restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
+    }
+
+    public ProjectInfo getProjectFromCache(long projectId) {
+        logger.info("Getting project with id {} from cache", projectId);
+        return projectsCache.get(projectId);
     }
 }
